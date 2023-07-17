e2e098c9df0aa65762021352319f6ae47f5bbf32
Christopher Cosma <cosma.5@osu.edu>
2023-07-17 15:50:40 -0400

    Added tailwind

--- frontend/app/src/app/components/header.component.ts
+++ frontend/app/src/app/components/header.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     template: `
     <header>
-        <h1>Intro to programming</h1>
+        <h1 class="text-3xl text-blue-800">Intro to programming</h1>
         <p>Sample Full-Stack Application For Class</p>
     </header>
     `
