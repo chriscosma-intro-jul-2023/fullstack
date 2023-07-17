d20d9c661c577038a077c4f3d88494b475932a61
Christopher Cosma <cosma.5@osu.edu>
2023-07-17 16:39:40 -0400

    Done with Monday

--- frontend/app/src/app/components/header.component.ts
+++ frontend/app/src/app/components/header.component.ts
@@ -4,8 +4,8 @@ import { Component } from "@angular/core";
     standalone: true,
     selector: 'app-header',
     template: `
-    <header>
-        <h1 class="text-3xl text-blue-800">Intro to programming</h1>
+    <header class="p-4 border-b-2 border-black">
+        <h1 class="text-3xl text-primary font-black">Intro to programming</h1>
         <p>Sample Full-Stack Application For Class</p>
     </header>
     `
---
+++ frontend/app/src/app/components/navigation.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+    template: `
+    <nav class="tabs tabs-boxed mb-4">
+        <a [routerLinkActive]="['tab-active']" routerLink="dashboard" class="tab tab-lg">Dashboard</a>
+        <a [routerLinkActive]="['tab-active']" routerLink="about" class="tab tab-lg">About</a>
+    </nav>
+    `,
+    standalone: true,
+    imports: [RouterLink, RouterLinkActive],
+    selector: 'app-navigation'
+})
+export class NavigationComponent {
+
+}
